24a698ce2efb0a073b14292b9f656725dc9f20ed
subzer288 <subzer288@hotmail.com>
2024-02-29 19:43:11 -0600

    Two Pointers

---
+++ src/ArraysAndStrings/ReverseString.java
@@ -0,0 +1,31 @@
+package ArraysAndStrings;
+
+//Write a function that reverses a string. The input string is given as an array of characters s.
+//
+//You must do this by modifying the input array in-place with O(1) extra memory.
+//
+//Example 1:
+//Input: s = ["h","e","l","l","o"]
+//Output: ["o","l","l","e","h"]
+//
+//Example 2:
+//Input: s = ["H","a","n","n","a","h"]
+//Output: ["h","a","n","n","a","H"]
+
+public class ReverseString {
+
+    public static void main(String[] arg){
+        char[] s = new char[]{'h','e','l','l','o'};
+        int left = 0;
+        int right = s.length-1;
+
+        while(left != right){
+            char aux = s[left];
+            s[left] = s[right];
+            s[right] = aux;
+            left++;
+            right--;
+        }
+        System.out.println(s);
+    }
+}
---
+++ src/ArraysAndStrings/SquaresofaSortedArray.java
@@ -0,0 +1,38 @@
+package ArraysAndStrings;
+import java.util.Arrays;
+// Given an integer array nums sorted in non-decreasing order,
+// return an array of the squares of each number sorted in non-decreasing order.
+//Example 1:
+//Input: nums = [-4,-1,0,3,10]
+//Output: [0,1,9,16,100]
+//Explanation: After squaring, the array becomes [16,1,0,9,100].
+//        After sorting, it becomes [0,1,9,16,100].
+
+//Example 2:
+//Input: nums = [-7,-3,2,3,11]
+//Output: [4,9,9,49,121]
+
+
+
+public class SquaresofaSortedArray {
+    public static void main (String arg[]){
+        int[] nums = new int[]{-4,-1,0,3,10};
+        int[] ans = new int[nums.length];
+        int left = 0;
+        int right = nums.length-1;
+
+        for(int i = right; i >=0 ; i--){
+            int square;
+            if(Math.abs(nums[left]) < Math.abs(nums[right])){
+                square = nums[right];
+                right--;
+            }else{
+                square = nums[left];
+                left++;
+            }
+            ans[i] = square * square;
+        }
+        System.out.println(Arrays.toString(ans));
+
+    }
+}
